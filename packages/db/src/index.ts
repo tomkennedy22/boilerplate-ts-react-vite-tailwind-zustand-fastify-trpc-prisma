df4485cdc9acb484469c6deaef8ed50ec645b219
Thomas Kennedy <tkennedy@smu.edu>
2024-11-03 09:54:32 -0600

    Prisma init

---
+++ packages/db/src/index.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+export * from "@prisma/client";
+
+export const extendedPrismaClient = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+}).$extends({
+  name: "extendedPrismaClient",
+  result: {
+    user: {
+      fullName: {
+        needs: { firstName: true, lastName: true },
+        compute(user) {
+          return `${user.firstName} ${user.lastName}`;
+        },
+      },
+    },
+  },
+});
